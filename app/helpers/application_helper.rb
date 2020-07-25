b44b4a999d4666c048d4c5e36557f6fd97a7ba2f
stobrien89 <stobrien@crimson.ua.edu>
2020-07-25 13:50:38 -0700

    add custom flash error/success messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
         gravatar_id = Digest::MD5::hexdigest(user.email).downcase
         "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
     end
+
+    def flash_class(level)
+        case level.to_sym
+            when :notice then "alert alert-warning"
+            when :success then "alert alert-success"
+            when :error then "alert alert-danger"
+            when :alert then "alert alert-danger"  
+        end
+    end
 end
