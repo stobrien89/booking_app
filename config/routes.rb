0a18b1a871328f7dca5546d91ff3489c56df00a1
stobrien89 <stobrien@crimson.ua.edu>
2020-07-31 17:49:40 -0700

    add host and guest review forms

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
 
-  resources guest_reviews, only: [:create, :destroy]
-  resources host_reviews, only: [:create, :destroy]
+  resources :guest_reviews, only: [:create, :destroy]
+  resources :host_reviews, only: [:create, :destroy]
 
   get '/your_trips' => 'reservations#your_trips'
   get '/your_reservations' => 'reservations#your_reservations'
