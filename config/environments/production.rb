9d2ed5d85fdb956eed07a327e66ad37d1ca0f972
stobrien89 <stobrien@crimson.ua.edu>
2020-08-08 03:17:28 -0700

    fix error in mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: => 'bookit-lify.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'bookit-lify.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   
@@ -122,5 +122,5 @@ Rails.application.configure do
     :user_name => ENV["mailgun_username"],
     :password => ENV["mailgun_password"],
   }
-  
+
 end
